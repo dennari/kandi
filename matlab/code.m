9971653c4f4d6443bd267ad79d60133dc947f85d
Ville Väänänen <ville.vaananen@verkkokauppa.com>
2010-03-22 03:28:09 +0200

    MidreturnII: got all the way up to and dealt with gaussian quadratures, new approach to the introduction, brought in some new references

--- matlab/code.m
+++ matlab/code.m
@@ -1,17 +1,19 @@
 %% Numeerisia integroimismenetelmiä
 
 f=@(x) exp(-x.^2);
-a=-2;
+a=0;
 b=2;
+sum_s = quad(f,a,b); % oikea arvo (Simpsonin säännöllä)
+
+
+
+
+%% Ylä ja alasummat
+
 n=20; % intervallien lukumäärä
 x=linspace(a,b,300);
 p=linspace(a,b,n+1);
 w = p(2)-p(1);
-sum_s = quad(f,a,b); % oikea arvo (Simpsonin säännöllä)
-
-%% Ylä ja alasummat
-
-
 
 sum_u = 0;
 sum_l = 0;
@@ -60,15 +62,16 @@ print(gcf, '-dpdf', '../fig_lusums.pdf');
 
 %% Puolisuunnikassääntö
 
-n=6; % intervallien lukumäärä
+n=2; % intervallien lukumäärä
 p=linspace(a,b,n+1);
 w = p(2)-p(1);
-
+sum_trapezoidal = 0;
 figure;
 hold on;
 
 for i=1:n
     h_t = area([p(i) p(i+1)],[f(p(i)) f(p(i+1))]);
+    sum_trapezoidal = 0.5*w*(f(p(i))+f(p(i+1)));  
     set(h_t,'FaceColor',[0.9 0.9 1]);
     set(h_t,'EdgeColor',[0 0 1]);
 end
@@ -77,6 +80,18 @@ h = plot(x,f(x),'b','LineWidth',2);
 
 hold off;
 legend([h h_t],'e^{-x^2}','puolisuunnikas');
+
+%% Simpsonin sääntö
+
+n=1; % intervallien lukumäärä
+p=linspace(a,b,2*n+1);
+w = p(2)-p(1);
+sum_simpson = 0;
+
+for i=1:n
+    sum_simpson = sum_simpson + (w/3)*(f(p(i))+4*f(p(i+1))+f(p(i+2)));  
+end
+
 
 
 %% print figure in pdf
@@ -91,3 +106,13 @@ set(gcf, 'PaperSize', [figLength figHeight]);
 set(gcf, 'PaperPositionMode', 'manual');
 set(gcf, 'PaperPosition', [0 0.01 figLength figHeight]);
 print(gcf, '-dpdf', '../fig_trapezoid.pdf');
+
+%% Kolmipisteinen Gaussin kvadratuuri välillä [a,b] ja painofunktiolla w=1
+
+Q3=@(f) 0.5*(b-a)*((5/9)*f(0.5*(b-a)*-sqrt(0.6)+0.5*(b+a))...
+        +(8/9)*f(0.5*(b+a))...
+        +(5/9)*f(0.5*(b-a)*sqrt(0.6)+0.5*(b+a)));
+disp(sprintf('quadrature: %.7f',Q3(f)));
+disp(sprintf('correct: %.7f',sqrt(pi)*erf(b)));
+disp(sprintf('error: %.7f',abs(sqrt(pi)*erf(b)-Q3(f))));
+
