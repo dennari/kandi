b6175108d763258f86db32fce54c48d46ceac1e7
Ville Väänänen <ville.vaananen@verkkokauppa.com>
2010-04-25 13:14:54 +0300

    test results

--- matlab/code.m
+++ matlab/code.m
@@ -121,38 +121,129 @@ disp(sprintf('error: %.7f',abs(multiplier*(Q3(f)-sum_s))));
 
 a = -1;
 b = 1;
-step = 0.1;
+step = 0.04;
+[X,Y] = meshgrid(a*2:step:b*2);
+% integroimisalue
+X1 = [-1 -1 1 -1; 1 -1 1 1; 1 -1 1 1; -1 -1 1 -1];
+Y1 = [-1 -1 -1 1; -1 1 1 1; -1 1 1 1; -1 -1 -1 1];
+Z1 =[0 0 0 0; 0 0 0 0; 1 1 1 1; 1 1 1 1];
+results = [];
+results2 = [];
+%figure;
+
+
+%% funktio 1 - gaussinen
+subplot(1,2,1);
+
 c = 0.7;
 f2=@(x,y,c) exp(-1*(2*(1-c^2))^-1*(x.^2+y.^2-2*c*x.*y));
 
 [cdf,cErr] = mvncdf([a a],[b b],[],[1 c; c 1],statset('TolFun',1e-14));
 correct = cdf*2*pi*sqrt(1-c^2);
+Z = f2(X,Y,c);
+surf(X,Y,Z,'FaceAlpha',0.8,'EdgeAlpha',0.3);
+%axis([-2 2 -2 2 -1 1]);
+%alpha(0.7);
+hold on;
+fill3(X1,Y1,max(max(Z))*Z1,'r','FaceAlpha',0.4,'EdgeAlpha',0.1);
+hold off;
+axis square;
+title('$$F_1=e^{-\frac{1}{1.02}\left(x^2+y^2-1.4xy\right)}$$','interpreter','latex','FontSize',14);
+%t=text(0.5,0.5,'$$e^{-\frac{1}{1.02}\left(x^2+y^2-1.4xy\right)}$$','interpreter','latex');
+%t=text(0.5,0.5,'e^{-1/1.02\left(x^2+y^2-1.4xy\right)}','interpreter','tex');
+%get(t);
+%% funktio 2 - itseisarvo
 
-[X,Y] = meshgrid(a*2:step:b*2);
+subplot(1,2,2);
+f3=@(x,y) abs(x.*y).^0.5;%+abs(x)-0.5*y;
+correct2 = dblquad(f3,a,b,a,b,1e-8);
+Z = f3(X,Y);
+h = surf(X,Y,Z,'FaceAlpha',0.8,'EdgeAlpha',0.3,'FaceColor','interp');
+shading faceted;
+%get(h);
+hold on;
+h = fill3(X1,Y1,max(max(Z))*Z1,'r','FaceAlpha',0.4,'EdgeAlpha',0.1);
 
-surf(X,Y,f2(X,Y,c));
+% line = [a a:step:b b];
+% const = ones(1,length(line));
+% X1 = [line; const; -const; line];
+% Y1 = [const; line; line; -const];
+% fV = f2(X1,Y1,c);
+% fV(:,1) = zeros(4,1);
+% fV(:,end)=zeros(4,1);
+% fill3(X1',Y1',1.4*fV','r','FaceAlpha',0.8,'EdgeAlpha',0.3);
+
+hold off;
+axis square;
+title('$$F_2=\sqrt{\vert xy \vert}$$','interpreter','latex','FontSize',14);
+%get(h);
+
+%% print figure in pdf
+
+%previewfig(gcf,'Color','rgb');
+%exportfig(gcf,'../fig_3dfunc.png','Format','png','Color','rgb','Renderer','opengl');
+print(gcf, '-djpeg100','-r350','../fig_3dfunc.jpg');
+%%
+figLength = 6;
+xyRatio = 16/10;
+figHeight = (1/xyRatio)*figLength;
+%axis([-2 2 0 1.02]);
+%set(gca, 'Position', get(gca, 'OuterPosition') - ...
+%    get(gca, 'TightInset') * [-1 0 1 0; 0 -1 0 1; 0 0 1 0; 0 0 0 1]*1.7);
+set(gcf, 'PaperUnits', 'inches');
+set(gcf, 'PaperSize', [figLength figHeight]);
+set(gcf, 'PaperPositionMode', 'manual');
+set(gcf, 'PaperPosition', [0 0.01 figLength figHeight]);
+print(gcf, '-dpdf', '../fig_3dfunc.pdf');
 
 %% 9-pisteinen tulosääntö
 
-prodRule = Q33(@(x,y)f2(x,y,c),a,b);
-fprintf('Q33: %.8f, err: %.8f\n',prodRule,abs(correct-prodRule));
+points = 3;
+prodRule2 = gauss2D(@(x,y)f2(x,y,c),points-1);
+err = abs(correct-prodRule2);
+relErr = (err/correct)*100;
+fprintf('gauss2D-%d: %.14f, err: %.14f\n',points^2,prodRule2,err);
+results(1,1:2)=[prodRule2 relErr];
 
 
-prodRule2 = gauss2D(@(x,y)f2(x,y,c),9);
-fprintf('gauss2D: %.14f, err: %.14f\n',prodRule2,abs(correct-prodRule2));
+prodRule2 = gauss2D(f3,points-1);
+err = abs(correct2-prodRule2);
+relErr = (err/correct)*100;
+fprintf('gauss2D-%d: %.14f, err: %.14f\n',points^2,prodRule2,err);
+results(1,3:4)=[prodRule2 relErr];
 
 
 %% radon 7-pisteinen
 
 radRule = radonSquare(@(x,y)f2(x,y,c));
+err = abs(correct-radRule);
+relErr = (err/correct)*100;
 fprintf('radon: %.5f, err: %.5f\n',radRule,abs(correct-radRule));
+results(2,1:2)=[radRule relErr];
+
+radRule = radonSquare(f3);
+err = abs(correct2-radRule);
+relErr = (err/correct)*100;
+fprintf('radon: %.5f, err: %.5f\n',radRule,abs(correct2-radRule));
+results(2,3:4)=[radRule relErr];
 
 %% minimaalinen d=19,N=68,S_2
 minim = minimal19(@(x,y)f2(x,y,c));
+err = abs(correct-minim);
+relErr = (err/correct)*100;
 fprintf('minimal19: %.12f, err: %.12f\n',minim,abs(correct-minim));
+results(3,1:2)=[minim relErr];
 
+minim = minimal19(f3);
+err = abs(correct2-minim);
+relErr = (err/correct)*100;
+fprintf('minimal19: %.12f, err: %.12f\n',minim,abs(correct2-minim));
+results(3,3:4)=[minim relErr];
 
+%% exporttaa tulokset
 
+matrix2latex(results,'~/Documents/School/Kandi/matlab/results1.tex','rowLabels',{'$Q_{3^2}$' '$Q_{7}$' '$Q_{68}$'},...
+    'columnLabels',{'$Q[F_1]$' '$E[F_1]$' '$Q[F_2]$' '$E[F_2]$'},'alignment','c','format',{'$%.5f$' '$%.1f\\%%$' '$%.5f$' '$%.1f\\%%$'});
 
 
 
